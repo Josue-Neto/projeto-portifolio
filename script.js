23f9bd90787a1d9971528ee7f8f0563d703fcd44
Josué Cardoso Martins Neto <josuemartinscardoso431@gmail.com>
2025-06-19 18:25:23 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function enviar(event) {
     event.preventDefault();
     const nome = document.getElementById('nome').value;
     const mensagem = document.getElementById('mensagem').value;
-    const telefone = '5511975649922';
+    const telefone = '5511989201761';
     const texto = `Olá! me chamo ${nome}, ${mensagem}`;
     const formatmsg = encodeURIComponent(texto);
     const url = `https://wa.me/${telefone}?text=${formatmsg}`;
